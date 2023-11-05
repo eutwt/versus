b4aa63a2681c68c71aeacfffaa23c66feb76e8c0
eutwt <11261404+eutwt@users.noreply.github.com>
2023-11-05 10:45:38 -0500

    don't worry about unload for now

--- R/versus.R
+++ R/versus.R
@@ -13,15 +13,5 @@
 "_PACKAGE"
 
 .onLoad <- function(libname, pkgname) {
-  old_option <- getOption("duckdb.materialize_message")
-  assign(".duckdb_materialize_old_option", old_option, envir = .GlobalEnv)
   options(duckdb.materialize_message = FALSE)
-}
-
-.onUnload <- function(libname) {
-  if (exists(".duckdb_materialize_old_option", envir = .GlobalEnv)) {
-    options(duckdb.materialize_message = get(".duckdb_materialize_old_option",
-                                             envir = .GlobalEnv))
-    rm(.duckdb_materialize_old_option, envir = .GlobalEnv)
-  }
 }
