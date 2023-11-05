fb03ce0503df10ccc0ee588ec9cf56d95524668d
eutwt <11261404+eutwt@users.noreply.github.com>
2023-11-04 20:01:54 -0400

    add merge_split

--- R/versus-func.R
+++ R/versus-func.R
@@ -18,3 +18,18 @@ versus <- function(table_a, table_b, by, allow_bothNA = TRUE, coerce = TRUE) {
 
 # Helpers ---------
 
+merge_split <- function(table_a, table_b, by) {
+  table_a$versus_in_a <- TRUE
+  table_b$versus_in_b <- TRUE
+  data <- full_join(table_a, table_b, by = by) %>%
+    mutate(across(starts_with('versus_in'), \(x) coalesce(x, FALSE)),
+           common = versus_in_a & versus_in_b)
+  common <- data %>%
+    filter(common) %>%
+    select(-starts_with('versus_in'), -common)
+  unmatched <- data %>%
+    filter(!common) %>%
+    mutate(table = if_else(versus_in_a, 'a', 'b'), .before = 1) %>%
+    select(table, all_of(by))
+  list(common = common, unmatched = unmatched)
+}
