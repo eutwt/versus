52dfdba4459ca0442cd5f5fa3a60f0a601dcdd32
eutwt <11261404+eutwt@users.noreply.github.com>
2023-12-19 22:17:37 -0500

    account for 0-row intersection

--- R/value-diffs.R
+++ R/value-diffs.R
@@ -73,7 +73,7 @@ value_diffs_all <- function(comparison) {
 
 stack_value_diffs <- function(comparison, column, pre_stack_fun, call = caller_env()) {
   column_locs <- get_cols_from_comparison(comparison, column, call = call)
-  is_selected <- seq(nrow(comparison$intersection)) %in% column_locs
+  is_selected <- seq_len(nrow(comparison$intersection)) %in% column_locs
   has_value_diffs <- comparison$intersection$n_diffs > 0
   to_stack <- which(is_selected & has_value_diffs) %0% which.max(is_selected)
 
