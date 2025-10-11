286d92695a771a5615b61d2e21bcfc0bc32ef6cd
eutwt <11261404+eutwt@users.noreply.github.com>
2025-10-11 12:34:00 -0400

    styler::style_pkg()

--- tests/testthat/test-get-diff-rows.R
+++ tests/testthat/test-get-diff-rows.R
@@ -1,11 +1,10 @@
-
 test_that("missing values are handled correctly", {
   x <- c(NA_real_, 1, 1, NA_real_, 1)
   y <- c(NA_real_, 1, 2, 1, NA_real_)
   expect_identical(not_equal(x, y, TRUE), 3:5)
 
-  x <- c(NA_real_, 1, 1, NA_real_, 1) %>% as.Date(origin = '1900-01-01')
-  y <- c(NA_real_, 1, 2, 1, NA_real_)  %>% as.Date(origin = '1900-01-01')
+  x <- c(NA_real_, 1, 1, NA_real_, 1) %>% as.Date(origin = "1900-01-01")
+  y <- c(NA_real_, 1, 2, 1, NA_real_) %>% as.Date(origin = "1900-01-01")
   expect_identical(not_equal(x, y, TRUE), 3:5)
 
   x <- c(NA_integer_, 1L, 1L, NA_integer_, 1L)
@@ -16,12 +15,12 @@ test_that("missing values are handled correctly", {
   y <- c(NA, TRUE, FALSE, TRUE, NA)
   expect_identical(not_equal(x, y, TRUE), 3:5)
 
-  x <- c(NA_character_, 'a', 'a', NA_character_, 'a')
-  y <- c(NA_character_, 'a', 'b', 'a', NA_character_)
+  x <- c(NA_character_, "a", "a", NA_character_, "a")
+  y <- c(NA_character_, "a", "b", "a", NA_character_)
   expect_identical(not_equal(x, y, TRUE), 3:5)
 
-  a <- as.POSIXct('1900-01-01', tz = "America/New_York")
-  b <- as.POSIXct('1900-01-01', tz = "UTC")
+  a <- as.POSIXct("1900-01-01", tz = "America/New_York")
+  b <- as.POSIXct("1900-01-01", tz = "UTC")
   na <- as.POSIXct(NA)
   x <- c(na, a, a, na, a)
   y <- c(na, a, b, a, na)
@@ -29,7 +28,7 @@ test_that("missing values are handled correctly", {
 })
 
 test_that("is_simple_class() works", {
-  ex <- as.POSIXct('1900-01-01', tz = "UTC")
+  ex <- as.POSIXct("1900-01-01", tz = "UTC")
   expect_identical(is_simple_class(1L, 1.1), FALSE)
   expect_identical(is_simple_class(1.1, 1.1), TRUE)
   expect_identical(is_simple_class(1L, 1L), TRUE)
